20f0c906fa6ed440385c8bd88bcb04a23eb8f729
Ryan Langewisch <rlangewi@gmail.com>
2020-09-08 22:01:26 -0600

    Added card refill files.

---
+++ Scripting Card Refill/global.lua
@@ -0,0 +1,7 @@
+DECK_GUID = "deb335"
+
+CARD_ZONE_GUIDS = {
+    "7ff48a",
+    "e4ede2",
+    "a42138"
+}
---
+++ Scripting Card Refill/refill-cards-button.lua
@@ -0,0 +1,26 @@
+DECK_GUID = Global.getVar('DECK_GUID')
+CARD_ZONE_GUIDS = Global.getTable('CARD_ZONE_GUIDS') 
+
+function onLoad()
+    cardZones = {}
+    for _, guid in ipairs(CARD_ZONE_GUIDS) do
+        zone = getObjectFromGUID(guid)
+        table.insert(cardZones, zone)
+    end
+
+    deck = getObjectFromGUID(DECK_GUID)
+end
+
+function refillCards()
+    for _, zone in ipairs(cardZones) do
+        if #zone.getObjects() == 0 then
+            deck.takeObject({flip=true, position=zone.getPosition()})
+        else
+            spawnObject({
+                type = "Chip_10",
+                position = zone.getPosition(),
+                scale = {0.5, 0.5, 0.5}
+            })
+        end
+    end
+end
